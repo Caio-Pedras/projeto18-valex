6b4604d1334ebbde0feb4278dde78cec0a45a9fc
CaioPedras <caio.vp@gmail.com>
2022-09-01 14:31:07 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, {json} from "express";
+import "express-async-errors"
+import cors from "cors";
+import dotenv from "dotenv"
+import router from "./routes/index.js"
+dotenv.config();
+
+
+const app=express();
+app.use(json());
+app.use(cors());
+app.use(router)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server up on port ${PORT}`)
+})
