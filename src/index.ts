4c2b17bc7e7f45cbb43e04106cd4c738ef099801
CaioPedras <caio.vp@gmail.com>
2022-09-01 21:49:38 -0300

    index config

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-import express, {json} from "express";
-import "express-async-errors"
+import express, { json } from "express";
+import "express-async-errors";
 import cors from "cors";
-import dotenv from "dotenv"
-import router from "./routes/index.js"
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+import errorHandler from "./middlewares/errorHandler.js";
 dotenv.config();
 
-
-const app=express();
+const app = express();
 app.use(json());
 app.use(cors());
-app.use(router)
+app.use(router);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`Server up on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server up on port ${PORT}`);
+});
